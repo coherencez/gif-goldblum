5c47c7c9a35b3b76cb6a0011bb5bb8b34c9c6240
Thomas Swann <tkswann@mac.com>
2016-09-08 23:07:38 -0500

    refactored parseJSON module

--- parseJSON.js
+++ parseJSON.js
@@ -5,11 +5,7 @@ module.exports = (array) => {
 	 ,	 appendString = '.gif'
 
   return array.map(v => v.url)
-							.map(v => v.slice(v.lastIndexOf('-')))
-							.map(v => {
-								if (v.charAt(0) === '-') {
-									return v.slice(1, v.length)
-								} else return v
-							})
-							.map(v => `${prependString}${v}${appendString}`)
+              .map(v => v.slice(v.lastIndexOf('-')))
+              .map(v => v.slice(1, v.length))
+              .map(v => `${prependString}${v}${appendString}`)
 }
