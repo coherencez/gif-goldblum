c5b860f36a4d665bcd31135f509b74fac8dbead6
Thomas Swann <tkswann@mac.com>
2016-09-30 21:29:40 -0500

    404 error handling

--- app.js
+++ app.js
@@ -27,8 +27,12 @@ app.use((req, res, cb) => {
   cb()
 })
 
+// ERROR HANDLING
 app.use((err,req,res,cb) => {
   res.status(500).send(`Internal Server Error ${err}`)
+})
+app.use((req, res, next) => {
+  res.status(404).send('Sorry cant find that!')
 })
 
 // ROUTES
