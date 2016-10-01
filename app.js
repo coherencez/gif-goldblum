8ff11d02ab93bf8262d1195b6f1f625422b09504
Thomas Swann <tkswann@mac.com>
2016-09-30 21:45:33 -0500

    removed 404 middleware, causing error on heroku

--- app.js
+++ app.js
@@ -31,9 +31,9 @@ app.use((req, res, cb) => {
 app.use((err,req,res,cb) => {
   res.status(500).send(`Internal Server Error ${err}`)
 })
-app.use((req, res, next) => {
-  res.status(404).send('Sorry cant find that!')
-})
+// app.use((req, res, next) => {
+//   res.status(404).send('Sorry cant find that!')
+// })
 
 app.get('/', (req,res) => {
   res.render('index')
